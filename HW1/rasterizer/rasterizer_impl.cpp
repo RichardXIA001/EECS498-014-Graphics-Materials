d87c7f2ca4e1873a4ea343726204746e9bf3021b
xingpeng <richardxia001@gmail.com>
2024-09-12 20:29:42 -0400

    HW1 Sec2

--- HW1/rasterizer/rasterizer_impl.cpp
+++ HW1/rasterizer/rasterizer_impl.cpp
@@ -4,6 +4,7 @@
 #include "loader.hpp"
 #include "rasterizer.hpp"
 #include <random>
+#include <iostream>
 // TODO
 bool IsPixelInsideTriangle(float x, float y, Triangle trig)
 {
@@ -43,7 +44,7 @@ std::vector<glm::vec3> GenerateSamplesInPixel(uint32_t x, uint32_t y, uint32_t s
     std::random_device rd;
     std::mt19937 gen(rd());  // Mersenne Twister random number engine
     std::uniform_real_distribution<float> dis(0.0f, 1.0f);  // Uniform distribution in range [0, 1]
-    // std::normal_distribution<float> dis(0.0f, 1.0f);  // Normal distribution with mean 0 and standard deviation 1
+    // std::normal_distribution<float> dis(0.5f, 1.0f);  // Normal distribution with mean 0 and standard deviation 1
     // Generate 'spp' samples
     for (uint32_t i = 0; i < spp; ++i)
     {
@@ -92,6 +93,26 @@ void Rasterizer::DrawPixel(uint32_t x, uint32_t y, Triangle trig, AntiAliasConfi
 void Rasterizer::AddModel(MeshTransform transform, glm::mat4 rotation)
 {
     /* model.push_back( model transformation constructed from translation, rotation and scale );*/
+    
+    // Translation matrix
+    glm::mat4 translation = glm::mat4(1.0f);
+    translation[3][0] = transform.translation.x;
+    translation[3][1] = transform.translation.y;
+    translation[3][2] = transform.translation.z;
+    
+    // Scale matrix
+    glm::mat4 scale = glm::mat4(1.0f);
+    scale[0][0] = transform.scale.x;
+    scale[1][1] = transform.scale.y;
+    scale[2][2] = transform.scale.z;
+
+    // glm::mat4 rot = glm::toMat4(transform.rotation);   // Quaternion to rotation matrix
+
+
+    // Model matrix
+    glm::mat4 model = translation * rotation * scale;
+    this->model.push_back(model);
+
     return;
 }
 
@@ -106,6 +127,34 @@ void Rasterizer::SetView()
     // TODO change this line to the correct view matrix
     this->view = glm::mat4(1.);
 
+    // Translate first
+    glm::mat4 translate = glm::mat4(1.0f);
+    translate[3][0] = -cameraPos.x;
+    translate[3][1] = -cameraPos.y;
+    translate[3][2] = -cameraPos.z;
+
+    // Rotate
+    // normailize the camera coordinate vector
+    glm::mat4 rotate = glm::mat4(1.0f);
+    cameraUp = glm::normalize(cameraUp);
+    glm::vec3 cameraView = glm::normalize(cameraLookAt - cameraPos);
+    
+    glm::vec3 auxiliary_axis = glm::cross(cameraView, cameraUp);
+
+    rotate[0][0] = auxiliary_axis.x;
+    rotate[1][0] = auxiliary_axis.y;
+    rotate[2][0] = auxiliary_axis.z;
+
+    rotate[0][1] = cameraUp.x;
+    rotate[1][1] = cameraUp.y;
+    rotate[2][1] = cameraUp.z;
+
+    rotate[0][2] = -cameraView.x;
+    rotate[1][2] = -cameraView.y;
+    rotate[2][2] = -cameraView.z;
+
+    this->view = rotate * translate;
+
     return;
 }
 
@@ -114,14 +163,39 @@ void Rasterizer::SetProjection()
 {
     const Camera& camera = this->loader.GetCamera();
 
-    float nearClip = camera.nearClip;                   // near clipping distance, strictly positive
-    float farClip = camera.farClip;                     // far clipping distance, strictly positive
+    float nearClip = -camera.nearClip;                   // near clipping distance, strictly positive
+    float farClip = -camera.farClip;                     // far clipping distance, strictly positive
     
-    float width = this->loader.GetWidth();
-    float height = this->loader.GetHeight();
+    float width = camera.width;
+    float height = camera.height;
     
     // TODO change this line to the correct projection matrix
-    this->projection = glm::mat4(1.);
+    this->projection = glm::mat4(1.0f);
+
+    glm::mat4 M_persp = glm::mat4(0.0f);
+
+    M_persp[0][0] = nearClip;
+    M_persp[1][1] = nearClip;
+    M_persp[2][2] = nearClip + farClip;
+    M_persp[3][2] = -nearClip * farClip;
+    M_persp[2][3] = 1.0f;
+
+    glm::mat4 M_ortho = glm::mat4(1.0f);
+
+    glm::mat4 M_scale = glm::mat4(1.0f);
+    glm::mat4 M_translate = glm::mat4(1.0f);
+
+    M_scale[0][0] = 2.0f / width;
+    M_scale[1][1] = 2.0f / height;
+    M_scale[2][2] = 2.0f / (nearClip - farClip);
+
+    M_translate[3][2] = -(farClip + nearClip) / 2.0f;
+    // M_translate[3][1] = - height / 2.0f;
+    // M_translate[3][0] = - width / 2.0f;
+    
+    M_ortho = M_scale * M_translate;
+
+    this->projection = M_ortho * M_persp;
 
     return;
 }
@@ -134,6 +208,12 @@ void Rasterizer::SetScreenSpace()
 
     // TODO change this line to the correct screenspace matrix
     this->screenspace = glm::mat4(1.);
+
+    this->screenspace[3][0] = width / 2.0f;
+    this->screenspace[3][1] = height / 2.0f;
+
+    this->screenspace[0][0] = width / 2.0f;
+    this->screenspace[1][1] = height / 2.0f;
 
     return;
 }
